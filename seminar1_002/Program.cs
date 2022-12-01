6645f25b246ed12621233e70b4b24e336018de9c
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-01 17:42:32 +0300

    seminar1_002

---
+++ seminar1_002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if(numB == Math.Sqrt(numA))
+{
+    Console.WriteLine("Да");
+}
+
+if(numB != Math.Sqrt(numA))
+{
+Console.WriteLine("Нет");
+}
