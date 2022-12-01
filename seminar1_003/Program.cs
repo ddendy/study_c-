41fbff6e180bd9b363349faa404f49ea41f417df
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-01 17:58:20 +0300

    seminar1_003

---
+++ seminar1_003/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Введите число, чтобы узнать день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+
+if(num == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+if(num == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+if(num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+
+if(num == 5)
+{
+    Console.WriteLine("Пятница");
+}
+
+if(num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+if(num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+if(num>7)
+{
+    Console.WriteLine("В неделе всего 7 дней!!!");
+}
+
+if(num<1)
+{
+    Console.WriteLine("В неделе нет нулевого дня!!!");
+}
