12f9179067645e159f4f4456f3b1daa58712521c
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-02 19:01:48 +0300

    hometask1_08

---
+++ hometask1_08/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = 1;
+
+while(n2 <= n1)
+    {
+        if(n2 % 2 ==0)
+        {
+           Console.Write($" {n2} "); 
+        }
+        n2++;
+    } 
