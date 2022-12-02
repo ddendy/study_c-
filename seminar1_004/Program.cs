570896158f419be785a51ed53675eb9c4f54ed21
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-02 12:58:03 +0300

    seminar1_004

---
+++ seminar1_004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = n1*-1;
+
+while(n2 <= n1)
+{
+    Console.Write($" {n2} ");
+    n2++;
+}
+Console.WriteLine();
