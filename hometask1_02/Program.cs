6408d4076e36036cfdeb8b37415b618439df2e0f
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-02 15:50:56 +0300

    hometask1_02

---
+++ hometask1_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if(numA > numB)
+{
+    Console.WriteLine($"число {numA} больше ");
+}
+
+if(numB > numA)
+{
+    Console.WriteLine($"число {numB} больше ");
+}
