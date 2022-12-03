8589b9d56870162177804670761e52dcec5cb60d
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-03 16:14:23 +0300

    seminar2_004

---
+++ seminar2_004/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+if(numA % 7 == 0 && numA % 23 == 0) Console.WriteLine("Да");
+else
+{
+    Console.WriteLine("Нет");
+}
