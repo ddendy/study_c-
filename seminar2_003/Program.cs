78a5aaea9903945451f7a99beb616047fb9b6a6b
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-03 16:05:39 +0300

    seminar2_003

---
+++ seminar2_003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+
+if(numA % numB == 0) Console.WriteLine("Первое число кратно второму");
+
+else
+{
+    int a1 = numA % numB;
+    Console.WriteLine($"Остаток от деления: {a1}");
+}
