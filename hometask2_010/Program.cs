936bde0c616b6733abcdec58d69b0366cc475fd6
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-06 23:27:42 +0300

    hometask2_010

---
+++ hometask2_010/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if(numA>100 && numA<999)
+{
+    int dif1 = numA / 100;
+    int dif2 = numA - dif1 * 100;
+    int dif3 = dif2 / 10;
+    Console.WriteLine($" Второе число цифры: {dif3}");
+}    
+else
+{
+    Console.WriteLine(" Число не трёхзначное!!!");
+}    
+
+
