d5a363148d327a190f59cab3e9c28c297d504389
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-07 00:46:55 +0300

    hometask2_013

---
+++ hometask2_013/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+string numA = Console.ReadLine();
+
+char[] nums = new char[numA.Length];
+
+if(numA.Length < 3)
+{
+    Console.WriteLine("Третьего числа нет!!!");
+}
+
+for(int i = 0; i < numA.Length; i++)
+{
+    nums[i] = numA[i];
+   
+    
+
+}
+Console.WriteLine($" Третье число заданной цифры: {nums[2]} ");
