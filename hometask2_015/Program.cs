7efe278cbf54635db187024088198025a776493e
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-08 17:48:18 +0300

    hometask2_015

---
+++ hometask2_015/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if(numA > 1 && numA < 8)
+{
+    
+}
+else  Console.WriteLine("Вы ввели неправильное число!");
+
+if(numA == 6) Console.WriteLine("Ура! Это выходной!");
+else if(numA == 7) Console.WriteLine("Ура! Это выходной!");
+else if(numA > 0 && numA < 6) Console.WriteLine("Это будний день");
+
+
+
