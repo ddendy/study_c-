1688d43adf6763340019798638da164245612c03
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-09 18:29:34 +0300

    hometask3_21

---
+++ hometask3_21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координату x точки A: ");
+double Ax = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y точки A: ");
+double Ay = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z точки A: ");
+double Az = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату x точки B: ");
+double Bx = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y точки B: ");
+double By = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z точки B: ");
+double Bz = Convert.ToDouble(Console.ReadLine());
+
+
+double length = Math.Sqrt(Math.Pow(Bx-Ax, 2)+Math.Pow(By-Ay, 2)+Math.Pow(Bz-Az, 2));
+Console.WriteLine($"Расстояние между точками = {length}");
