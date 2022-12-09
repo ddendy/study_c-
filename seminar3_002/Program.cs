bc77450ab5fa23fa69a36c33b3db1f58551853cd
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-09 13:58:26 +0300

    seminar3_002

---
+++ seminar3_002/Program.cs
@@ -0,0 +1,14 @@
+﻿string dot(int quater)
+{
+   string koord;
+   if(quater == 1) koord = "x>0, y>0";
+   else if(quater == 2) koord = "x>0, y<0";
+   else if(quater == 3) koord = "x<0, y>0";
+   else if(quater == 4) koord = "x<0, y<0";
+   else koord = "введите число от 1 до 4";
+   return koord;
+}
+
+Console.WriteLine("Введите число обозначающее четверть(от 1 до 4): ");
+int quater = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(dot(quater));
