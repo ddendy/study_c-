dc650289d8cea15f3c4833204cd0b993ea8da48d
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-09 14:58:00 +0300

    seminar3_003

---
+++ seminar3_003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите координату x точки A: ");
+double Ax = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y точки A: ");
+double Ay = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату x точки B: ");
+double Bx = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y точки B: ");
+double By = Convert.ToDouble(Console.ReadLine());
+
+double length = Math.Sqrt(Math.Pow(Ax-Bx, 2)+Math.Pow(Ay-By, 2));
+Console.WriteLine($"Расстояние между точками = {length}");
