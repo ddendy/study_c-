b08399350cecefbdb8dee61518d39497abcc5114
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-09 17:11:02 +0300

    hometask3_023

---
+++ hometask3_023/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if(num<0) num=-num;
+
+while(i<=num)
+{
+    int j = Convert.ToInt32(Math.Pow(i, 3));
+    Console.Write($" {j} ");
+    i++;
+}
