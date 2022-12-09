6e912fb225a8e7cd81c4f8a207734cae7125731c
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-09 15:22:47 +0300

    seminar3_004

---
+++ seminar3_004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while(i<num)
+{
+    int j = Convert.ToInt32(Math.Pow(i, 2));
+    Console.Write($" {j} ");
+    i++;
+}
