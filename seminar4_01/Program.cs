711e522016c3dd45a1c0fc44b160c76624aee7db
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-13 12:22:48 +0300

    seminar4_01

---
+++ seminar4_01/Program.cs
@@ -0,0 +1,13 @@
+﻿int sumNumbers(int num)
+{
+    int sum = 0;
+    for(int i = 1; i <= num; i++) sum+=i;
+    return sum;
+}
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// for(int i = 1; i <= num; i++) sum+=i;
+Console.WriteLine($"Сумма от 1 до {num} равна {sumNumbers(num)}");
