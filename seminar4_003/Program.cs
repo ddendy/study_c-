7aa4c87b5db4c19eb5eb7d9da820856047f20a63
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-15 19:58:36 +0300

    seminar4_003

---
+++ seminar4_003/Program.cs
@@ -0,0 +1,17 @@
+﻿int multiple(int numA)
+{
+    int f = 1;
+    for (int i = 1; i <= numA; i++)
+    {
+        f = f * i;
+    }
+    return f;
+}
+
+Console.WriteLine("Введите число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(multiple(numA));
+
+
+
+
