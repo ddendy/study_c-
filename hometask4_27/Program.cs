ce5897ce691f8825c1d8f79315f79e27c7445860
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-15 21:20:27 +0300

    hometask4_27

---
+++ hometask4_27/Program.cs
@@ -0,0 +1,13 @@
+﻿int sum(int numA)
+{
+    int f = 0;
+    for (int i = 0; i <= numA; i++)
+    {
+        f = f + i;
+    }
+    return f;
+}
+
+Console.WriteLine("Введите число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(numA));
