ddacc4d8860c8c371715fc453466650b9a392663
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-15 20:44:13 +0300

    seminar4_04

---
+++ seminar4_04/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите длину массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = new int[n];
+
+for(int i = 0; i<n; i++)
+{
+    array[i] = new Random().Next(0,2);
+    Console.Write($" {array[i]} ");
+}
