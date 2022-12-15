2aebcc4853b917f51e6da2829a038ce6352f3b61
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-15 21:14:26 +0300

    hometask4_25

---
+++ hometask4_25/Program.cs
@@ -0,0 +1,17 @@
+﻿int exponent(int n)
+{
+    Console.WriteLine("Введите степень:");
+    int i = Convert.ToInt32(Console.ReadLine());
+    int num = n;
+    for(int j = 2; j <= i; j++)
+    {
+        num = num*n;
+    }
+    return num;
+}
+
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(exponent(n));
