69d03dd0c0f73e428884b1fb4bd7a00a29c6e62c
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-15 19:18:55 +0300

    seminar4_02

---
+++ seminar4_02/Program.cs
@@ -0,0 +1,15 @@
+﻿int amount(int num)
+{
+    int count = 0;
+    while (num >= 1)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+
+}
+
+Console.WriteLine("Введите число:");
+double num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество цифр в числе: {amount(num)}");
