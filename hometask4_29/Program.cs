a9b86ef8fa5191705bce98b9feacc2969f144482
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-15 21:31:38 +0300

    hometask4_29

---
+++ hometask4_29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите числа массива: ");
+string numA = Console.ReadLine();
+
+char[] nums = new char[numA.Length];
+for(int i = 0; i < numA.Length; i++)
+{
+    nums[i] = numA[i];
+    Console.Write($" {nums[i]} ");
+}
+
