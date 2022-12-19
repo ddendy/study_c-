903b164c052c228f28ff4f7e60fe01f1a9b4c9ae
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-19 20:04:26 +0300

    seminar5_01

---
+++ seminar5_01/Program.cs
@@ -0,0 +1,38 @@
+﻿// int[] NewArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(-100, 100);
+//     }
+//     return array;
+// }
+
+// int[] ChangeArray(int[] array)
+// {
+//     for (int g = 0; g < size; g++)
+//     {
+//         array[g] = array[g] * -1;
+//         Console.Write($"Изменённый массив: {array[g]} ");
+//     }
+//     return array;
+// }
+
+
+Console.WriteLine("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+    Console.Write($" {array[i]} ");
+}
+
+
+for (int g = 0; g < size; g++)
+{
+    array[g] = array[g] * -1;
+    Console.WriteLine($" {array[g]} ");
+}
