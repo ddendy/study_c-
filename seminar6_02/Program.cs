76790bafd20ef8154c3e489811294e958c722a69
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-29 15:56:53 +0300

    seminar6_02

---
+++ seminar6_02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+string res = "";
+
+while (num1 > 0)
+{
+    res = Convert.ToString(num1%2) + res;
+    num1 = num1 / 2;
+    
+}    
+Console.WriteLine(res);
