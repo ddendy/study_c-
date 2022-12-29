c8d9e95bc40d0b8d86e506d173923d3cc84e424b
Daniil Kantser <kancerdanya20001@gmail.com>
2022-12-29 13:28:21 +0300

    seminar6_01

---
+++ seminar6_01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 <= num2+num3 && num2 <= num1+num3 && num3 <= num1+num2) Console.WriteLine("Это треуольник!!!");
+else Console.WriteLine("Это не может быть треугольником!");
