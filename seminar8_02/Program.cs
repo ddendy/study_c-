e83391825b0fb3aa4850dc3881111320bdf19953
Daniil Kantser <kancerdanya20001@gmail.com>
2023-01-15 14:27:37 +0300

    seminar8_02

---
+++ seminar8_02/Program.cs
@@ -0,0 +1,46 @@
+﻿void ReverseArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = i; j< array.GetLength(1); j++)   
+        (array[i,j], array[j,i]) = (array[j,i], array[i,j]);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j< array.GetLength(1); j++)   
+        {
+            Console.Write($"{array[i,j],3}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(-20,21);
+        }
+
+    }
+}
+
+Console.WriteLine("Введите количество строк массива:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива:");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+if(rows != cols) Console.WriteLine("Невозможно поменять столбцы и строки местами!");
+else
+{
+    ReverseArray(array);
+    Console.WriteLine();
+    PrintArray(array);
+}
+
