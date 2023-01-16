1c81dffdae2aaa168ce1072b03f2d15cdca410bb
Daniil Kantser <kancerdanya20001@gmail.com>
2023-01-16 19:19:14 +0300

    seminar9_01

---
+++ seminar9_01/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите целое число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"сумма чисел от 1 до {n} равна {Summa(n)}");
+Console.WriteLine($"сумма чисел от 1 до {n} равна {SummaRec(n)}");
+
+int Summa(int n)
+{
+    int res = 0;
+    while (true)
+    {
+        res+=n;
+        n--;
+        if(n==0) break;
+    }
+    return res;
+}
+
+int SummaRec(int n)
+{
+    if(n==0) return 0;
+    return n + SummaRec(n-1);
+}
