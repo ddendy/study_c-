426cad9e9cfda6713b6a7cedc0abb29d88c6891b
Daniil Kantser <kancerdanya20001@gmail.com>
2023-01-16 23:25:07 +0300

    hometask9_66

---
+++ hometask9_66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите левую границу промежутка:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите правую границу промежутка:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"сумма чисел в промежутке от {m} до {n} равна: {SumRec(m, n)}");
+
+int SumRec(int m, int n)
+{
+    if(n==(m-1)) return 0;
+    return SumRec(m, n-1) + n;
+}
