594fee843704cb322c3ca9d54f6451eae5cae907
Daniil Kantser <kancerdanya20001@gmail.com>
2023-01-16 22:57:31 +0300

    seminar9_04

---
+++ seminar9_04/Program.cs
@@ -0,0 +1,10 @@
+﻿int SumNum(int m)
+{
+    if(m==0) return 0;
+    return SumNum(m/10) +m%10;
+}
+
+Console.WriteLine("Введите число:");
+int m = Convert.ToInt32(Console.ReadLine());
+int sum = SumNum(m);
+Console.WriteLine($"Сумма цифр числа = {sum}");
