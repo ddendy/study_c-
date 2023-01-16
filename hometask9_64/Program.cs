b60a378f28498edd8be9c815e39050769542d38a
Daniil Kantser <kancerdanya20001@gmail.com>
2023-01-16 23:17:49 +0300

    hometask9_64

---
+++ hometask9_64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"числа в промежутке от {n} до 1: {ListRec(n)}");
+
+
+string ListRec(int n)
+{
+    if(n==0) return "";
+    return n + ", " + ListRec(n-1);
+}
