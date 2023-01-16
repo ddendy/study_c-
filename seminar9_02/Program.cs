9bf05d1c804f111c832734ca05919ee02bbc299a
Daniil Kantser <kancerdanya20001@gmail.com>
2023-01-16 20:10:55 +0300

    seminar9_02

---
+++ seminar9_02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"числа в промежутке от 1 до {n}: {SummaRec(n)}");
+
+
+string SummaRec(int n)
+{
+    if(n==0) return "";
+    return SummaRec(n-1) + n + ", ";
+}
